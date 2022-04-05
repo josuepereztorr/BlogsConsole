44d88e05fcfe640dd50829250c882efbdc2ab771
Josue <josuepereztorr@gmail.com>
2022-04-04 22:21:20 -0500

    - Try Catch - Exception

--- Program.cs
+++ Program.cs
@@ -7,10 +7,21 @@ namespace BlogsConsole
     {
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         private static readonly BloggingContext Context = new BloggingContext();
+        
         static void Main(string[] args)
         {
             Logger.Info("Program started");
-            BloggingController controller = new BloggingController(Context, Logger);
+
+            try
+            {
+                BloggingController controller = new BloggingController(Context, Logger);
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception.Message);
+            }
+            
+            Logger.Info("Program ended");
         }
     }
 }
